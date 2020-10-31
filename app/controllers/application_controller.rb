e5976af4aa4cbadb3e54334023ea2c7d0c69c1d6
LOÏC RICO <ricoloic4@gmail.com>
2020-10-31 17:33:23 -0400

    employee acess to backoffice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
 
   skip_before_action :verify_authenticity_token
   def after_sign_in_path_for(resource)
-    if current_user.is_admin?
+    if current_user.is_admin? || current_user.is_employee?
       rails_admin_path
     else
       new_quote_path
