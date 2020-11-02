d14e7c9dc7e18e9737b9a31f9d8779257940f8dd
LOÏC RICO <ricoloic4@gmail.com>
2020-11-02 16:20:48 -0500

    sdhbfikuysdh

--- app/controllers/elevators_controller.rb
+++ app/controllers/elevators_controller.rb
@@ -1,40 +1,42 @@
 class ElevatorsController < ApplicationController
 
     def edit
-        @elevator = Elevator.find(params[:status])
+      @elevator = Elevator.edit
+
+      @elevator = Elevator.find(params[:status])
+      puts "here is something !!!!!!!!!!!!!!!!!!!!!!"
     
-=begin         old_status = @elevator.status
+      old_status = @elevator.status
     
-        if @elevator.update(elevator_params)
-          send_sms
-          redirect_to elevator_path(@elevator.id)
-        else
-          render :edit
-        end
+      if @elevator.update(elevator_params)
+        send_sms old_status
+        redirect_to elevator_path(@elevator.id)
+      else
+        render :edit
       end
+    end
     
-      private
-    
-      def elevator_params
-        params.require(:elevator).permit(:status)
-      end
-    
-      def send_sms(old_status)
-        require("bundler")
-        Bundler.require()
-        
-        account_sid = ENV["TWILIO_ACCOUNT_SID"]
-        auth_token = ENV["TWILIO_AUTH_TOKEN"]
-        
-        @client = Twilio::REST::Client.new(account_sid, auth_token)
-        
-        @client.messages.create(
-            to: '+18198171295',
-            from: '+12058929412',
-            body: "Hi this is a message from Rocket Elevators, telling you that a lead has been created!"
-        )
-      end 
-=end
-puts "EDIT BEING MADE!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    private
+  
+    def elevator_params
+      params.require(:elevator).permit(:status)
+    end
+  
+    def send_sms(old_status)
+      puts "SENDDDDDIIIIIINNNNNGGGGGG"
+
+      # require("bundler")
+      # Bundler.require()
+      
+      # account_sid = ENV["TWILIO_ACCOUNT_SID"]
+      # auth_token = ENV["TWILIO_AUTH_TOKEN"]
+      
+      # @client = Twilio::REST::Client.new(account_sid, auth_token)
+      
+      # @client.messages.create(
+      #     to: '+18198171295',
+      #     from: '+12058929412',
+      #     body: "Hi this is a message from Rocket Elevators, telling you that a lead has been created!"
+      # )
     end
 end
--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,3 +1,22 @@
 class Elevator < ApplicationRecord
   belongs_to :column
+
+  after_update do |elev|
+    puts ""
+  end
+=begin 
+  require("bundler")
+  Bundler.require()
+
+  account_sid = 'ACab428b7e54e6aa4f3ffd74b2b92d5f08'
+  auth_token = '3dd8a31b56f9ff6cd5e61be216546a99'
+      
+  @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+  @client.messages.create(
+      to: '+18198171295',
+      from: '+12058929412',
+      body: "Hi this is a message from Rocket Elevators, telling you that a lead has been created!"
+  )
+=end
 end
