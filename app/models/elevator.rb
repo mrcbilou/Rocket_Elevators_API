20cd162527b87b79483652a9228d6539a46a054f
mrcbilou <louisfelix95@hotmail.com>
2020-11-03 13:51:37 -0500

    slack working

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -4,11 +4,20 @@ require 'slack-notifier'
 class Elevator < ApplicationRecord
   belongs_to :column
 
-  old_status =""
+ 
 
-  before_save do |elev|
-    old_status = elev.elevator_status
-    puts "11111!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#{old_status}!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  around_update :send_slack_notif
+
+  private
+
+  def send_slack_notif
+    status_changed = self.elevator_status_changed?
+
+    if status_changed 
+      notifier = Slack::Notifier.new "https://hooks.slack.com/services/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"
+      notifier.ping "The Elevator ##{self.id} with Serial Number #{self.serial_number} changed status from **#{self.elevator_status_was}** to **#{self.elevator_status}**"
+    end
+    yield
   end
 
   after_update do |elev|
@@ -27,7 +36,6 @@ class Elevator < ApplicationRecord
           body: "Hi this is a message from Rocket Elevators. Elevator ##{elev.id} now has the status of #{elev.elevator_status}"
       )
     end
-    notifier = Slack::Notifier.new "https://hooks.slack.com/services/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"
-    notifier.ping "The Elevator ##{elev.id} with Serial Number #{elev.serial_number} changed status from #{self.elevator_status_was} to #{elev.elevator_status}"
+   
   end
 end
