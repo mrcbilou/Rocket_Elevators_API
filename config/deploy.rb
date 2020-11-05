d9fa86a71da53c1607de8b501ff77c08e906cd3d
LOÏC RICO <ricoloic4@gmail.com>
2020-11-05 13:09:10 -0500

    change dloyment name

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,8 @@ lock "~> 3.14.1"
 
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
-set :application, "team_will_sinclair"
-set :repo_url, "https://github.com/WilliamSinclairF/Rocket_Elevators_Information_System"
+set :application, "LoicRico"
+set :repo_url, "https://github.com/ricoloic/Rocket_Elevators_API.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
