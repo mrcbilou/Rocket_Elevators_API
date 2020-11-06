5b1905243efcf3fac1483c96c93a2b451152607b
LOÏC RICO <ricoloic4@gmail.com>
2020-11-05 23:42:08 -0500

    working sendgrid

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -13,15 +13,45 @@ class LeadsController < ApplicationController
 
         @lead.save
 
+        sendMail
+
         respond_to do |format|
             if @lead.save && user_signed_in?
                 format.html { redirect_to my_leads_path, notice: 'Your lead as been successfully registered !' }
-
             elsif @lead.save && !user_signed_in?
                 format.html { redirect_to root_path, notice: 'Your lead as been successfully registered !' }
             else
                 format.html { render :new }
             end
+        end
+    end
+
+    def sendMail
+        email = @lead.email
+        full_name = @lead.full_name
+        project_name = @lead.project_name
+        phone = @lead.phone
+
+        mail = SendGrid::Mail.new
+        mail.from = SendGrid::Email.new(email: "rocketelevatorswk7@gmail.com")
+
+        personalization = SendGrid::Personalization.new
+        personalization.add_to(SendGrid::Email.new(email: email))
+        personalization.add_dynamic_template_data({
+            "full_name" => full_name,
+            "project_name" => project_name,
+            "phone" => phone
+        })
+
+        mail.add_personalization(personalization)
+        mail.template_id = 'd-15d6bef02786488fa205bd75c1fa8f51'
+
+        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+        
+        begin
+            response = sg.client.mail._('send').post(request_body: mail.to_json)
+        rescue Message.e
+            pp e.message
         end
     end
 
--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -5,6 +5,7 @@ class Elevator < ApplicationRecord
   belongs_to :column
   
   around_update :send_slack_notif
+  after_update :twilio_txt
 
   private
 
@@ -18,24 +19,8 @@ class Elevator < ApplicationRecord
     yield
   end
 
- 
-
-  around_update :send_slack_notif
-
-  private
-
-  def send_slack_notif
-    status_changed = self.elevator_status_changed?
-
-    if status_changed 
-      notifier = Slack::Notifier.new ENV["slack_hook"]
-      notifier.ping "The Elevator ##{self.id} with Serial Number #{self.serial_number} changed status from **#{self.elevator_status_was}** to **#{self.elevator_status}**"
-    end
-    yield
-  end
-
-  after_update do |elev|
-    if elev.elevator_status == "Intervention"
+  def twilio_txt
+    if self.elevator_status == "Intervention"
       require("bundler")
       Bundler.require()
 
@@ -47,9 +32,8 @@ class Elevator < ApplicationRecord
       @client.messages.create(
         to: ENV['RECIPIENT_PHONE_NUMBER'],
         from: '+12058929412',
-        body: "Hi this is a message from Rocket Elevators. Elevator ##{elev.id} now has the status of #{elev.elevator_status}"
+        body: "Hi this is a message from Rocket Elevators. Elevator ##{self.id} now has the status of #{self.elevator_status}"
       )
     end
-   
   end
 end
