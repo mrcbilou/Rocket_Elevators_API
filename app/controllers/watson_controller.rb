3218185d9cecd2348e0ef56008be5d6eda2d92cc
AnthonyPageau <antopageau@hotmail.com>
2020-11-05 20:08:01 -0500

    IBM done

--- app/controllers/watson_controller.rb
+++ app/controllers/watson_controller.rb
@@ -7,14 +7,16 @@ class WatsonController < ActionController::Base
     def speak
   
         authenticator = Authenticators::IamAuthenticator.new(
-            apikey: 
+            apikey: ""
         )
         text_to_speech = TextToSpeechV1.new(
             authenticator: authenticator
         )
             
-        text_to_speech.service_url =
+        text_to_speech.service_url = ""
         
+        first_name = current_user.first_name
+        last_name = current_user.last_name
         elevators = Elevator.count
         buildings = Building.count
         customers = Customer.count
@@ -22,10 +24,10 @@ class WatsonController < ActionController::Base
         quotes = Quote.count
         leads = Lead.count
         batteries = Battery.count
-        cities = Address.count(:city)
+        cities = Address.distinct.count(:city)
 
         response = text_to_speech.synthesize(
-            text: "Greetings. There are currently #{elevators} elevators deployed in the #{buildings} buildings of your #{customers} customers.",
+            text: "Greetings #{first_name} #{last_name}. There are currently #{elevators} elevators deployed in the #{buildings} buildings of your #{customers} customers. Currently, #{not_active_elevators} elevators are not in Running Status and are being serviced. You currently have #{quotes} quotes awaiting processing. You currently have #{leads} leads in your contact requests. #{batteries} Batteries are deployed across #{cities} cities",
             accept: "audio/mp3",
             voice: "en-GB_KateV3Voice"
         ).result
